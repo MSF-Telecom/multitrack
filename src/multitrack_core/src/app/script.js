439bb85ff163e06e419798f30f5b62a0494f609f
Frédéric Druppel <Frederic.druppel@icloud.com>
2025-09-30 09:21:13 +0200

    removed other debug marker

--- src/multitrack_core/src/app/script.js
+++ src/multitrack_core/src/app/script.js
@@ -1,9 +1,5 @@
 console.log('Hello from script.js!')
 
-const popup2 = new maplibregl.Popup({ offset: 25 }).setHTML(
-  'Test popup2 : <a href="http://crouton.net">link</a>'
-);
-
 // let radios = {
 //   "main-id_sn0000": {
 //     "main_ID": "main-id",
@@ -230,11 +226,3 @@ function setMarkerColor(marker, color) {
 };
 
 updateControls();
-
-setTimeout(() => {
-  markers["WolStPi"] = new maplibregl.Marker()
-    .setLngLat([4.46464, 50.8335])
-    .setPopup(popup2)
-    .addTo(map);
-  updateMarkers();
-}, 1000);
